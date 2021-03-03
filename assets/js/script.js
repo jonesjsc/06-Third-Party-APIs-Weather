b25513654658ba5579128f9d522d4dd5424ef49a
Jason Jones <jason.e.jones@gmail.com>
2021-03-03 17:49:20 -0500

    chained 2 fetches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 console.log("running");
-apiUrl = "http://api.openweathermap.org/data/2.5/weather";
+const apiUrl = "http://api.openweathermap.org/data/2.5/weather";
+const apiUrl2 = "http://api.openweathermap.org/data/2.5/forecast";
 const APPID = "3fa7b2c1cbc30cd3a3619a313b9a2946";
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=22443fc9195b77b16450ff1e7196b692
-
-
-
+// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3fa7b2c1cbc30cd3a3619a313b9a2946
+// api.openweathermap.org/data/2.5/forecast?id=524901&appid=3fa7b2c1cbc30cd3a3619a313b9a2946
+// problems: 
+// 1. how to clear the search that the user typed in
+// 2. how to execute the search if they hit ENTER key OR click on the search icon
+// 3. how to capture the value that they entered and THEN call the fetch
+//
 function getSearchResults () {
 $(document).on("keypress", "input", function(e){
     if(e.which == 13){
@@ -14,22 +18,54 @@ $(document).on("keypress", "input", function(e){
         document.getElementById('city-search').value = '';
         console.log ("clear");
     }
-    return(searchString)
-});
-}
-// var r = getSearchResults;
-// console.log (r);
+    // return(searchString)
+    })
+};
+
+getSearchResults();
+
 var searchString = "rock hill";
-fetchUrl=apiUrl+"?q="+searchString+"&APPID="+APPID;
-fetch (fetchUrl, {
+var latCity;
+var lonCity;
+var idCity;
+var humidity;
+var windspeed;
+var UVindex;
+var fiveDay = [] // date cloud temp humidity
+
+
+let fetchUrl=apiUrl+"?q="+searchString+"&APPID="+APPID;
+
+var array = fetch (fetchUrl, {
     method: 'GET', //GET is the default.
     credentials: 'same-origin', // include, *same-origin, omit
     redirect: 'follow', // manual, *follow, error
 })
 .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
+    console.log ("then response");
+    return response.json();    
+})
+.then(function (data) {
+    console.log ("data here");
     console.log(data);
-  });
+    
+    let idCity = data.id;
+    console.log ("city name is "+data.name+" lat "+latCity+" lon "+lonCity);
+    
+    let fetchUrl=apiUrl2+"?id="+idCity+"&APPID="+APPID;
+console.log (fetchUrl);
+    var array = fetch (fetchUrl, {
+        method: 'GET', //GET is the default.
+        credentials: 'same-origin', // include, *same-origin, omit
+        redirect: 'follow', // manual, *follow, error
+    })
+.then(function (response) {
+        console.log ("then response");
+        return response.json();    
+})
+.then(function (data) {
+        console.log ("data here");
+        console.log(data);
+})
+});
 
