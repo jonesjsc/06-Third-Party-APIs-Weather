786dc3a04305079dbe546810dd336ecfa38ea198
Jason Jones <jason.e.jones@gmail.com>
2021-03-06 21:28:42 -0500

    error checking and alerting

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # 06-Third-Party-APIs-Weather
+Due Date: 3/6/2021 @ 11:59 PM
+Coding Bootcamping assigned #6 - Third party APIs / Weather
+
+# Objectives
+The primary objectives of this exercise were to demonstrate the following:
+
+* use of third party API
+* use of styling framework (Bumla)
+* DOM manipulation
+* DOM traversing
+* dynamic HTML from Javascript
+* local storage
+
+# References / Resources 
+
+|Description|Link|
+|-----------|----|
+|Template literals|https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals|
+
+# Screenshots
+### Screen 1: **Initial Screen**
+![<img src=assets/images/screen01.png>](assets/images/screen01.png)
+
+This is a simple calendar appointment entry exercise.
+
+### Screen 2: **Different Features**
+![<img src=assets/images/screen02.png>](assets/images/screen02.png)
+
+1. Using moment.js, wdisplay the current Date.
+2. When the calendar is rendered, we need to colarize it.  Compare the current time to the time being renders.  This is the block of the calendar for time that is before the current hour.
+3. This is the hour line stylized for the current hour
+4. This is how the calendar is stylized for time in the future.
+5. This is the save button.
+
+### Screen 3: **Data Entry**
+![<img src=assets/images/screen03.png>](assets/images/screen03.png)
+
+Here is a view of the screen during data entry.
+
+
+### Screen 4: **Post Save**
+![<img src=assets/images/screen04.png>](assets/images/screen04.png)
+
+This is a view once data has been inputted and saved.
+
+## Links
+* This repo is located [on github](https://github.com/jonesjsc/05-Third-Party-APIs-Calendar)
+* This site is deployed on [github pages](https://jonesjsc.github.io/05-Third-Party-APIs-Calendar/)
+
+## License
+* ![License](https://img.shields.io/github/license/jonesjsc/05-Third-Party-APIs-Calenda)
+
+## Fun Stats
+* ![Last Commit](https://img.shields.io/github/last-commit/jonesjsc/03-JavaScript-Password-Generator)
+* ![Current Understanding level](https://img.shields.io/badge/Understanding%20Level-Gettin%20There-yellow)
--- assets/js/script.js
+++ assets/js/script.js
@@ -52,8 +52,25 @@ var lonCity;
 var humidity;
 var windspeed;
 var UVindex;
-var fiveDay = [] // date cloud temp humidity
- 
+var isValidSearch = true;
+
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+    
+function showAlert (message, className) {
+    const div = document.createElement('div');
+    div.className = `text-is-centered message is-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.box');
+    const search = document.querySelector('#search-box');
+    container.insertBefore(div, search);
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.message').remove(),3000);
+}
 
 function searchWeather (searchString) {
 
@@ -63,152 +80,166 @@ function searchWeather (searchString) {
     // this is the first fetch - we get lat and lon from this fetch
     //
 
-    var array = fetch (fetchUrl, {
+   fetch (fetchUrl, {
         method: 'GET', //GET is the default.
         credentials: 'same-origin', // include, *same-origin, omit
         redirect: 'follow', // manual, *follow, error
     })
+    .then(handleErrors)
     .then(function (response) {
         return response.json();    
     })
-    .then(function (data) {       
-        let latCity = data.coord.lat;
-        let lonCity = data.coord.lon;   
-        let fetchUrl=apiUrl3+"?lat="+latCity+"&lon="+lonCity+"&units=imperial&APPID="+APPID;
-    
-        //
-        // this is the subsequent fetch to get 5 day, and current uv
-        //
+    .catch(error => {
+        console.log("yo dawg "+error) 
+        isValidSearch=false;
 
-        var array = fetch (fetchUrl, {
-            method: 'GET', //GET is the default.
-            credentials: 'same-origin', // include, *same-origin, omit
-            redirect: 'follow', // manual, *follow, error
-        })
-    .then(function (response) {
-            return response.json();    
+        showAlert ("invalid city name","danger");
+
+        return;
     })
-    .then(function (data2) {
-            //
-            // there is some formatting that operweathermap does on the search string, so lets ride that 
-            // and set what we display based on what the city name we get back
-            //
-            var searchString2 = data.name; 
-            let iconUrl="http://openweathermap.org/img/wn/"+data2.current.weather[0].icon+"@2x.png";
+    .then(function (data) {      
+        if (isValidSearch) {
 
-            //
-            // let's write out the current weather
-            //
+            let latCity = data.coord.lat;
+            let lonCity = data.coord.lon;   
 
-            $("#current-weather").html(`
-            <div class="title is-4">
-                ${data.name} (${moment(data2.daily[0].dt, 'X').format('LL')}) 
-                <img src=${iconUrl} alt="Weather Icon" class="smaller-icon">
-            </div>
-            `);
-
-            //
-            // What were doing here is color coding the UV Index.  Here's how
-            // First lets build an array called uvScale and in index 0 and 1 will be the class name for green
-            // in that array we'll put yellow in 2 3 4, and 5 thru 10 will be red.
-            // Then - we're gonna round the actual current uvi, and then just use that as the index on the 
-            // array for the class name
-            //
-
-            var uvScale = ["uvi-green", "uvi-green", "uvi-yellow", "uvi-yellow", "uvi-yellow", "uvi-red", "uvi-red", "uvi-red", "uvi-red", "uvi-red", "uvi-red"];
-            var uvClass = uvScale[Math.round(data2.current.uvi)];
+            let fetchUrl=apiUrl3+"?lat="+latCity+"&lon="+lonCity+"&units=imperial&APPID="+APPID;
         
-            $("#current-weather").append(`
-            <div>
-                Temperature: ${Math.round(data2.current.temp)} &#8457<br>
-                Humidify: ${data2.current.humidity} %<br>
-                Wind Speed: ${Math.round(data2.current.wind_speed)} MPH<br>
-                UV Index: <span class="${uvClass}">&nbsp;&nbsp;&nbsp;${data2.current.uvi}&nbsp;&nbsp;&nbsp;</span>
-            </div>
-            `);
-
             //
-            // let's start by clearing out the five day forecast block
+            // this is the subsequent fetch to get 5 day, and current uv
             //
 
-            $("#five-day-forecast").html(``);
+            fetch (fetchUrl, {
+                method: 'GET', //GET is the default.
+                credentials: 'same-origin', // include, *same-origin, omit
+                redirect: 'follow', // manual, *follow, error
+            })
+            .then(function (response) {
+                return response.json();    
+            })
+            .then(function (data2) {
+                //
+                // there is some formatting that operweathermap does on the search string, so lets ride that 
+                // and set what we display based on what the city name we get back
+                //
+                var searchString2 = data.name; 
+                let iconUrl="http://openweathermap.org/img/wn/"+data2.current.weather[0].icon+"@2x.png";
 
-            //
-            // since we only want 5 days, and since index 0 is the current day, lets
-            // loop 1 through 6
-            //
+                //
+                // let's write out the current weather
+                //
 
-            for (let i = 1; i < 6; i++ ) {
-                $("#five-day-forecast").append(` 
-                    <div class="card">
-                        <div class="card-content">
-                            <div class="content">
-                                <div class="is-size-6">
-                                    ${moment(data2.daily[i].dt, 'X').format('L')}<br>
-                                </div>
-                                <div class="">
-                                    <img src="http://openweathermap.org/img/wn/${data2.daily[i].weather[0].icon}@2x.png" alt="Weather Icon" class="smaller-icon"><br>
-                                </div>
-                                <div class="is-size-7">
-                                    Temp: ${Math.round(data2.daily[i].temp.day)} &#8457<br>
-                                    Humidity: ${data2.daily[i].humidity} %
+                $("#current-weather").html(`
+                <div class="title is-4">
+                    ${data.name} (${moment(data2.daily[0].dt, 'X').format('LL')}) 
+                    <img src=${iconUrl} alt="Weather Icon" class="smaller-icon">
+                </div>
+                `);
+
+                //
+                // What were doing here is color coding the UV Index.  Here's how
+                // First lets build an array called uvScale and in index 0 and 1 will be the class name for green
+                // in that array we'll put yellow in 2 3 4, and 5 thru 10 will be red.
+                // Then - we're gonna round the actual current uvi, and then just use that as the index on the 
+                // array for the class name
+                //
+
+                var uvScale = ["uvi-green", "uvi-green", "uvi-yellow", "uvi-yellow", "uvi-yellow", "uvi-red", "uvi-red", "uvi-red", "uvi-red", "uvi-red", "uvi-red"];
+                var uvClass = uvScale[Math.round(data2.current.uvi)];
+            
+                $("#current-weather").append(`
+                <div>
+                    Temperature: ${Math.round(data2.current.temp)} &#8457<br>
+                    Humidify: ${data2.current.humidity} %<br>
+                    Wind Speed: ${Math.round(data2.current.wind_speed)} MPH<br>
+                    UV Index: <span class="${uvClass}">&nbsp;&nbsp;&nbsp;${data2.current.uvi}&nbsp;&nbsp;&nbsp;</span>
+                </div>
+                `);
+
+                //
+                // let's start by clearing out the five day forecast block
+                //
+
+                $("#five-day-forecast").html(``);
+
+                //
+                // since we only want 5 days, and since index 0 is the current day, lets
+                // loop 1 through 6
+                //
+
+                for (let i = 1; i < 6; i++ ) {
+                    $("#five-day-forecast").append(` 
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="content">
+                                    <div class="is-size-6">
+                                        ${moment(data2.daily[i].dt, 'X').format('L')}<br>
+                                    </div>
+                                    <div class="">
+                                        <img src="http://openweathermap.org/img/wn/${data2.daily[i].weather[0].icon}@2x.png" alt="Weather Icon" class="smaller-icon"><br>
+                                    </div>
+                                    <div class="is-size-7">
+                                        Temp: ${Math.round(data2.daily[i].temp.day)} &#8457<br>
+                                        Humidity: ${data2.daily[i].humidity} %
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                `);
-                }
-                
-                //
-                // let's load the LOCAL STORAGE search history
-                //
+                    `);
+                    }
+                    
+                    //
+                    // let's load the LOCAL STORAGE search history
+                    //
 
-                var searchHistory = JSON.parse(localStorage.getItem("MY_WEATHER_APP_SEARCH_HISTORY"));
+                    var searchHistory = JSON.parse(localStorage.getItem("MY_WEATHER_APP_SEARCH_HISTORY"));
 
-                //
-                // It looks dumb to keep up with the same search multiple times, so lets unshift only unique searches
-                //
-                if(jQuery.inArray(searchString2, searchHistory) == -1) {
-                    searchHistory.unshift(searchString2);
-                } 
-                else {
-                    searchHistory.splice(jQuery.inArray(searchString2, searchHistory),1);
-                    // then put it back on at the top
-                    searchHistory.unshift(searchString2);
-                }
+                    //
+                    // It looks dumb to keep up with the same search multiple times, so lets unshift only unique searches
+                    //
+                    if(jQuery.inArray(searchString2, searchHistory) == -1) {
+                        searchHistory.unshift(searchString2);
+                    } 
+                    else {
+                        searchHistory.splice(jQuery.inArray(searchString2, searchHistory),1);
+                        // then put it back on at the top
+                        searchHistory.unshift(searchString2);
+                    }
 
-                //
-                // We cant keep everything - so lets just keep maxSearchHistory number of historical searches
-                //
+                    //
+                    // We cant keep everything - so lets just keep maxSearchHistory number of historical searches
+                    //
 
-                if (searchHistory.length > maxSearchHistory) {
-                    searchHistory.pop();
-                }
-                
-                //
-                // Update local storage with current searchHistory
-                //
+                    if (searchHistory.length > maxSearchHistory) {
+                        searchHistory.pop();
+                    }
+                    
+                    //
+                    // Update local storage with current searchHistory
+                    //
 
-                localStorage.setItem("MY_WEATHER_APP_SEARCH_HISTORY", JSON.stringify(searchHistory));
+                    localStorage.setItem("MY_WEATHER_APP_SEARCH_HISTORY", JSON.stringify(searchHistory));
 
-                //
-                // render the search history box by first clearing the search history box out
-                //
+                    //
+                    // render the search history box by first clearing the search history box out
+                    //
 
-                $("#search-history").html(``);
+                    $("#search-history").html(``);
 
-                //
-                // lets loop through all the elements in the searchHistory array and render the search box
-                //
+                    //
+                    // lets loop through all the elements in the searchHistory array and render the search box
+                    //
 
-                searchHistory.forEach(function(searchItem) {
-                    $("#search-history").append(` 
-                    <div>
-                        <button class="button makeit-100">
-                            ${searchItem}
-                        </button>
-                    </div>`);
-                });  
-        })
+                    searchHistory.forEach(function(searchItem) {
+                        $("#search-history").append(` 
+                        <div>
+                            <button class="button makeit-100">
+                                ${searchItem}
+                            </button>
+                        </div>`);
+                    });  
+            })
+        } // otherwise don't do anything because it was an invalid search
+        isValidSearch = true; // toggle to try again
     });
 }
